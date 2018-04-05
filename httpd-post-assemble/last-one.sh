1d1996f6e242d24652057a421bbc8e4b6229cb26
lucari <richard@lucari.co.uk>
2018-04-05 17:28:41 +0100

    Update last-one.sh

--- httpd-post-assemble/last-one.sh
+++ httpd-post-assemble/last-one.sh
@@ -2,12 +2,4 @@
 
 echo "last-one.sh has been run under httpd-post-assemble"
 
-PODHOST=`hostname`
-
-cat >showme.html <<EOF
-<HTML><HEAD><TITLE>Show who I am page</TITLE></HEAD>
-<BODY>
-My hostname is $PODHOST
-</BODY>
-</HTML>
-EOF
+ln -s /data1/www/special special
