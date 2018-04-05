c994d93af7fc78c68448f66865cc91fe3fcad00f
lucari <richard@lucari.co.uk>
2018-04-05 17:19:05 +0100

    Update last-one.sh

--- httpd-post-assemble/last-one.sh
+++ httpd-post-assemble/last-one.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "last-one.sh has been run under httpd-post-assemble"
+
+PODHOST=`pwd`
+
+>showme.html <<EOF
+<HTML><HEAD><TITLE>Show who I am page</TITLE></HEAD>
+<BODY>
+My hostname is $PODHOST
+</BODY>
+</HTML>
+EOF
